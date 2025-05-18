64ac5972b8655446a70989991a8929aff41cd679
Minecatl1 <140106910+Minecatl1@users.noreply.github.com>
2025-05-18 12:21:50 -0500

    Update fetch-deps.sh

--- scripts/fetch-deps.sh
+++ scripts/fetch-deps.sh
@@ -1,11 +1,23 @@
 #!/bin/bash
 set -e
 
-# Use partial ISO download to save space
-wget --no-check-certificate \
-    --header="Range: bytes=0-100000000" \
-    -O partial.iso \
-    https://releases.ubuntu.com/22.04.3/ubuntu-22.04.3-desktop-amd64.iso
+# Use Ubuntu Minimal CD (69MB) as base
+BASE_URL="http://archive.ubuntu.com/ubuntu/dists/jammy/main/installer-amd64/current/legacy-images/netboot/mini.iso"
 
-# Extract just the essential boot files
-7z x -obuild partial.iso '[BOOT]'
+echo "Downloading minimal base image..."
+wget -O base.iso "$BASE_URL"
+
+echo "Extracting kernel..."
+mkdir -p build/boot/grub
+7z x -obuild base.iso linux initrd.gz
+mv build/linux build/boot/vmlinuz
+mv build/initrd.gz build/boot/initrd.img
+
+echo "Preparing GRUB config..."
+cat > build/boot/grub/grub.cfg <<EOL
+set timeout=10
+menuentry "MikaOS" {
+    linux /boot/vmlinuz root=/dev/sda1 ro quiet splash
+    initrd /boot/initrd.img
+}
+EOL
