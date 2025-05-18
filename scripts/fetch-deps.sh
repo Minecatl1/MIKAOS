fb523dd48426fa39a568f77c3d3cc73b9ad62a60
Minecatl1 <140106910+Minecatl1@users.noreply.github.com>
2025-05-18 14:25:31 -0500

    Update fetch-deps.sh

--- scripts/fetch-deps.sh
+++ scripts/fetch-deps.sh
@@ -1,29 +1,32 @@
 #!/bin/bash
 set -eo pipefail
 
-# Create optimized UEFI boot image
-grub-mkimage -O x86_64-efi \
-    -p /boot/grub \
-    -c config/grub-uefi.cfg \
+# Create initramfs directory structure
+mkdir -p build/etc/initramfs-tools/{conf.d,hooks,scripts}
+cp -r /etc/initramfs-tools/* build/etc/initramfs-tools/ || true
+
+# Generate basic initramfs config
+cat > build/etc/initramfs-tools/conf.d/default <<EOL
+MODULES=dep
+COMPRESS=gzip
+EOL
+
+# Create UEFI boot image
+grub-mkstandalone -O x86_64-efi \
     -o build/EFI/efiboot.img \
-    part_gpt part_msdos fat ext2 iso9660 linux normal \
-    search_fs_uuid search_fs_file ls chain echo configfile
-
-# Create optimized BIOS bootloader
-grub-mkimage -O i386-pc \
-    -p /boot/grub \
-    -c config/grub-bios.cfg \
-    -o build/boot/grub/core.img \
-    biosdisk part_msdos iso9660 linux normal \
-    search_fs_uuid search_fs_file ls chain echo configfile
-
-# Combine BIOS components
-cat /usr/lib/grub/i386-pc/cdboot.img build/boot/grub/core.img > build/boot/grub/stage2_eltorito
+    --modules="part_gpt part_msdos" \
+    --locales="" \
+    --themes="" \
+    /boot/grub/grub.cfg=config/grub-uefi.cfg
 
 # Get custom kernel
 wget -O linux_binary_cache.zip https://github.com/Minecatl1/linux_binary_cache/archive/refs/tags/1.0.zip
 unzip -j linux_binary_cache.zip "linux_binary_cache-1.0/vmlinuz-5.15.0-105" -d build/boot/
 mv build/boot/vmlinuz-5.15.0-105 build/boot/vmlinuz
 
-# Create minimal initrd
+# Create initrd using host config if missing
+if [ ! -f build/etc/initramfs-tools/initramfs.conf ]; then
+    cp /etc/initramfs-tools/initramfs.conf build/etc/initramfs-tools/
+fi
+
 mkinitramfs -d build/etc/initramfs-tools -o build/boot/initrd.img
