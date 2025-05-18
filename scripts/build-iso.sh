d6ff9e5163585b1ba620e6b687b8108412b3b43f
Minecatl1 <140106910+Minecatl1@users.noreply.github.com>
2025-05-18 13:10:01 -0500

    Update build-iso.sh

--- scripts/build-iso.sh
+++ scripts/build-iso.sh
@@ -1,12 +1,16 @@
 #!/bin/bash
 set -e
 
-echo "Building hybrid ISO..."
 xorriso -as mkisofs \
-  -volid "MikaOS" \
-  -eltorito-boot boot/grub/stage2_eltorito \
-  -no-emul-boot -boot-load-size 4 -boot-info-table \
-  -o output/mikaos.iso \
-  build
+    -volid "MIKAOS" \
+    -rational-rock \
+    -joliet \
+    -b boot/grub/stage2_eltorito \
+    -no-emul-boot \
+    -boot-load-size 4 \
+    -boot-info-table \
+    -input-charset utf-8 \
+    -o output/mikaos.iso \
+    build
 
-echo "ISO created at output/mikaos.iso"
+isohybrid --uefi output/mikaos.iso
