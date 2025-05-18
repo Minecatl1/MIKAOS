dee589ea48693824fb3efc925333860c8b35389d
Minecatl1 <140106910+Minecatl1@users.noreply.github.com>
2025-05-18 13:54:23 -0500

    Update fetch-deps.sh

--- scripts/fetch-deps.sh
+++ scripts/fetch-deps.sh
@@ -1,24 +1,26 @@
 #!/bin/bash
 set -eo pipefail
 
-# Get BIOS bootloader
-wget -O build/boot/grub/stage2_eltorito \
-    https://github.com/littleosbook/littleosbook/raw/master/files/stage2_eltorito
-
 # Create UEFI boot image
-mkdir -p build/EFI
 grub-mkstandalone -O x86_64-efi \
-    -o build/EFI/efiboot.img \
     --modules="part_gpt part_msdos" \
-    --locales="" \
+    --install-modules="normal echo linux ls chain" \
     --themes="" \
-    /boot/grub/grub.cfg=config/grub-uefi.cfg
+    --fonts="" \
+    --locales="" \
+    -o build/EFI/efiboot.img \
+    "boot/grub/grub.cfg=config/grub-uefi.cfg"
+
+# BIOS bootloader
+grub-mkstandalone -O i386-pc \
+    --modules="part_msdos" \
+    -o build/boot/grub/stage2_eltorito \
+    "boot/grub/grub.cfg=config/grub-bios.cfg"
 
 # Get custom kernel
-KERNEL_ZIP="linux_binary_cache-1.0.zip"
-wget -O "${KERNEL_ZIP}" https://github.com/Minecatl1/linux_binary_cache/archive/refs/tags/1.0.zip
-unzip -j "${KERNEL_ZIP}" "linux_binary_cache-1.0/vmlinuz-5.15.0-105" -d build/boot/
+wget -O linux_binary_cache.zip https://github.com/Minecatl1/linux_binary_cache/archive/refs/tags/1.0.zip
+unzip -j linux_binary_cache.zip "linux_binary_cache-1.0/vmlinuz-5.15.0-105" -d build/boot/
 mv build/boot/vmlinuz-5.15.0-105 build/boot/vmlinuz
 
-# Create initrd
+# Generate initramfs
 mkinitramfs -o build/boot/initrd.img
