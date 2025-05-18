a958ca45683d954cdb366ab8af9cecea3a0983c9
Minecatl1 <140106910+Minecatl1@users.noreply.github.com>
2025-05-18 13:47:02 -0500

    Update fetch-deps.sh

--- scripts/fetch-deps.sh
+++ scripts/fetch-deps.sh
@@ -1,28 +1,24 @@
 #!/bin/bash
 set -eo pipefail
 
-# Get GRUB stage2_eltorito
+# Get BIOS bootloader
 wget -O build/boot/grub/stage2_eltorito \
-    https://github.com/littleosbook/littleosbook/raw/refs/heads/master/files/stage2_eltorito
+    https://github.com/littleosbook/littleosbook/raw/master/files/stage2_eltorito
 
-# Get custom kernel from ZIP
+# Create UEFI boot image
+mkdir -p build/EFI
+grub-mkstandalone -O x86_64-efi \
+    -o build/EFI/efiboot.img \
+    --modules="part_gpt part_msdos" \
+    --locales="" \
+    --themes="" \
+    /boot/grub/grub.cfg=config/grub-uefi.cfg
+
+# Get custom kernel
 KERNEL_ZIP="linux_binary_cache-1.0.zip"
-KERNEL_DIR="linux_binary_cache-1.0"
-
-echo "Downloading custom kernel..."
 wget -O "${KERNEL_ZIP}" https://github.com/Minecatl1/linux_binary_cache/archive/refs/tags/1.0.zip
-unzip -o "${KERNEL_ZIP}"
+unzip -j "${KERNEL_ZIP}" "linux_binary_cache-1.0/vmlinuz-5.15.0-105" -d build/boot/
+mv build/boot/vmlinuz-5.15.0-105 build/boot/vmlinuz
 
-echo "Installing kernel..."
-if [ -f "${KERNEL_DIR}/vmlinuz-5.15.0-105" ]; then
-    mv "${KERNEL_DIR}/vmlinuz-5.15.0-105" build/boot/vmlinuz
-else
-    echo "ERROR: Kernel not found in archive!"
-    exit 1
-fi
-
-echo "Creating initrd..."
+# Create initrd
 mkinitramfs -o build/boot/initrd.img
-
-# Cleanup
-rm -rf "${KERNEL_ZIP}" "${KERNEL_DIR}"
