4fc24f2b554b010becccb1140a2a9a06a32b8d92
Minecatl1 <140106910+Minecatl1@users.noreply.github.com>
2025-05-18 14:38:56 -0500

    Update fetch-deps.sh

--- scripts/fetch-deps.sh
+++ scripts/fetch-deps.sh
@@ -1,32 +1,21 @@
 #!/bin/bash
 set -eo pipefail
 
-# Create initramfs directory structure
-mkdir -p build/etc/initramfs-tools/{conf.d,hooks,scripts}
-cp -r /etc/initramfs-tools/* build/etc/initramfs-tools/ || true
-
-# Generate basic initramfs config
-cat > build/etc/initramfs-tools/conf.d/default <<EOL
-MODULES=dep
-COMPRESS=gzip
+# Create initramfs config with device discovery
+mkdir -p build/etc/initramfs-tools
+cat > build/etc/initramfs-tools/conf.d/root <<EOL
+MODULES=most
+BOOT=local
+DEVICE=eth0
+ROOT=UUID=00000000-0000-0000-0000-000000000000
+ROOTFSTYPE=ext4
 EOL
 
-# Create UEFI boot image
-grub-mkstandalone -O x86_64-efi \
-    -o build/EFI/efiboot.img \
-    --modules="part_gpt part_msdos" \
-    --locales="" \
-    --themes="" \
-    /boot/grub/grub.cfg=config/grub-uefi.cfg
+# Force include virtio drivers for cloud environments
+echo "virtio_pci virtio_blk virtio_net" >> build/etc/initramfs-tools/modules
 
-# Get custom kernel
-wget -O linux_binary_cache.zip https://github.com/Minecatl1/linux_binary_cache/archive/refs/tags/1.0.zip
-unzip -j linux_binary_cache.zip "linux_binary_cache-1.0/vmlinuz-5.15.0-105" -d build/boot/
-mv build/boot/vmlinuz-5.15.0-105 build/boot/vmlinuz
-
-# Create initrd using host config if missing
-if [ ! -f build/etc/initramfs-tools/initramfs.conf ]; then
-    cp /etc/initramfs-tools/initramfs.conf build/etc/initramfs-tools/
-fi
-
-mkinitramfs -d build/etc/initramfs-tools -o build/boot/initrd.img
+# Generate initrd with dummy root
+mkinitramfs -d build/etc/initramfs-tools \
+    -k build/boot/vmlinuz \
+    -o build/boot/initrd.img \
+    --force
