4a994ccb675a95c798da3bc26309a4762f135890
Minecatl1 <140106910+Minecatl1@users.noreply.github.com>
2025-05-18 12:00:33 -0500

    Update fetch-deps.sh

--- scripts/fetch-deps.sh
+++ scripts/fetch-deps.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
 set -e
 
-# Download Ubuntu base ISO (4GB)
-wget -O base.iso 'https://releases.ubuntu.com/22.04.3/ubuntu-22.04.3-desktop-amd64.iso'
+# Use partial ISO download to save space
+wget --no-check-certificate \
+    --header="Range: bytes=0-100000000" \
+    -O partial.iso \
+    https://releases.ubuntu.com/22.04.3/ubuntu-22.04.3-desktop-amd64.iso
 
-# Extract kernel from ISO
-7z x -obuild base.iso '[BOOT]/filesystem.squashfs'
-unsquashfs -d build/filesystem build/filesystem.squashfs
-
-# Get UEFI components
-wget -O build/EFI/efiboot.img https://github.com/linux-surface/linux-surface/releases/download/silverblue-20230313/edk2-ovmf.snap
+# Extract just the essential boot files
+7z x -obuild partial.iso '[BOOT]'
