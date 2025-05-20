8899178f1b5c6776811cc6eacdf672e92bf94a2a
Minecatl1 <140106910+Minecatl1@users.noreply.github.com>
2025-05-19 19:51:54 -0500

    Update build-iso.sh

--- scripts/build-iso.sh
+++ scripts/build-iso.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 set -e
 
-# Generate module dependencies for custom kernel
-depmod -b build $(basename build/boot/vmlinuz | sed 's/vmlinuz-//')
-
 xorriso -as mkisofs \
-    -volid "MIKAOS" \
-    -rational-rock \
-    -joliet \
-    -b boot/grub/stage2_eltorito \
-    -no-emul-boot \
-    -boot-load-size 4 \
-    -boot-info-table \
-    -o output/mikaos.iso \
-    build
+  -volid "MIKAOS" \
+  -rational-rock \
+  -joliet \
+  -b boot/grub/stage2_eltorito \
+  -no-emul-boot \
+  -boot-load-size 4 \
+  -boot-info-table \
+  -eltorito-alt-boot \
+  -e EFI/efiboot.img \
+  -no-emul-boot \
+  -o output/mikaos.iso \
+  build
