8e5078d316aca1f894776dc9406279c7357fb751
Minecatl1 <140106910+Minecatl1@users.noreply.github.com>
2025-05-19 19:52:46 -0500

    Update fetch-deps.sh

--- scripts/fetch-deps.sh
+++ scripts/fetch-deps.sh
@@ -1,26 +1,51 @@
 #!/bin/bash
 set -eo pipefail
 
-# Create directory structure
+# Create initramfs structure
 mkdir -p build/etc/initramfs-tools/{conf.d,hooks,scripts}
-mkdir -p build/boot/grub
-
-# Create minimal initramfs.conf
 cat > build/etc/initramfs-tools/initramfs.conf <<EOL
 MODULES=most
 BUSYBOX=auto
 COMPRESS=gzip
 DEVICE=eth0
-ROOT=UUID=00000000-0000-0000-0000-000000000000
 EOL
 
-# Create modules file
-echo "virtio_pci virtio_blk virtio_net" > build/etc/initramfs-tools/modules
+# Network drivers
+cat > build/etc/initramfs-tools/modules <<EOL
+e1000
+r8169
+ath9k
+iwlwifi
+rt2800usb
+usbnet
+ax88179_178a
+nvme
+ahci
+i915
+EOL
 
 # Get custom kernel
-wget -O linux_binary_cache.zip https://github.com/Minecatl1/linux_binary_cache/archive/refs/tags/1.0.zip
+wget -q --show-progress -O linux_binary_cache.zip \
+  https://github.com/Minecatl1/linux_binary_cache/archive/refs/tags/1.0.zip
 unzip -j linux_binary_cache.zip "linux_binary_cache-1.0/vmlinuz-5.15.0-105" -d build/boot/
 mv build/boot/vmlinuz-5.15.0-105 build/boot/vmlinuz
+
+# Build UEFI GRUB
+grub-mkstandalone -O x86_64-efi \
+  -o build/EFI/efiboot.img \
+  --modules="part_gpt part_msdos fat ext2" \
+  --locales="" \
+  --themes="" \
+  "boot/grub/grub.cfg=config/grub-uefi.cfg"
+
+# Build BIOS GRUB
+grub-mkstandalone -O i386-pc \
+  -o build/boot/grub/core.img \
+  --modules="biosdisk part_msdos iso9660" \
+  --locales="" \
+  --themes="" \
+  "boot/grub/grub.cfg=config/grub-bios.cfg"
+cat /usr/lib/grub/i386-pc/cdboot.img build/boot/grub/core.img > build/boot/grub/stage2_eltorito
 
 # Generate initrd
 mkinitramfs -d build/etc/initramfs-tools -o build/boot/initrd.img
