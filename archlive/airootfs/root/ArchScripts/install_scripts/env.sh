de52f301087e63067957366dbb550fc37b81f816
Minecatl1 <140106910+Minecatl1@users.noreply.github.com>
2026-04-19 14:13:48 -0500

    Add environment configuration script for installation

---
+++ archlive/airootfs/root/ArchScripts/install_scripts/env.sh
@@ -0,0 +1,13 @@
+# Settings for your installed system
+# Hint: To exit vim, press ESC + :wq + ENTER
+
+USERNAME="Minecatl1"            # Your username
+PASSWORD="arch"                 # Your password
+HOSTNAME="archiso"              # The hostname of this particular system
+USERSHELL="/usr/bin/fish"            # Default shell to be used by root and user
+LANGUAGE="en_US"                # Check /etc/locale.gen for more options
+KEYMAP="us"                     # Check /usr/share/kbd/keymaps/**/*.map.gz for more options
+ZONEINFO="America/Chicago"     # Check /usr/share/zoneinfo/ for more options
+DESKTOP_ENV="xfce4"               # kde, gnome
+BOOTLOADER="grub"             # refind, refind-fallback, grub
+VIDEO_DRIVERS="default"         # default, nvidia
