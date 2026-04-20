fbc8ac5f0b9d7293f0dfb9f11c852343a0643350
Minecatl1 <140106910+Minecatl1@users.noreply.github.com>
2026-04-20 02:19:37 +0000

    make it work

--- makeiso.sh
+++ makeiso.sh
@@ -2,7 +2,7 @@
 docker build -t mikaos-achiso-builder .
 echo "Build completed. Starting container to build ISO..."
 sleep 3
-docker run --privileged -d --name mikaos-iso-builder   -v "$PWD":/workspace   -v "$HOME/arch/iso":/iso-output   -w /workspace   mikaos-iso-builder
+docker run --privileged -d --name mikaos-iso-builder   -v "$PWD":/workspace   -v "$HOME/arch/iso":/iso-output   -w /workspace   mikaos-achiso-builder
 echo "Container started. Building ISO inside container..."
 echo "View logs with: docker logs -f mikaos-iso-builder"
 echo "Once the build is complete, the ISO will be available in the 'arch/iso' directory on your host machine."
